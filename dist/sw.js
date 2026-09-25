65efc223767fc7c773d2785a0c468e1fc540eb86
chrisbrouard <chrisbrouard@yahoo.com>
2026-09-25 15:57:19 -0400

    Widen overlapping neighborhood heat zones

--- dist/sw.js
+++ dist/sw.js
@@ -1,4 +1,4 @@
-const CACHE='home-base-v52';
+const CACHE='home-base-v53';
 const ASSETS=['./','index.html','manifest.webmanifest','favicon.svg','icon-192.png','icon-512.png','homebase-logo.png','baltimore-map.jpg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
